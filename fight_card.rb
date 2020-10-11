e08127a4d06df956da2afc065faecc6d6ed15939
joaofelipe <joaofelipe1294@gmail.com>
2020-10-11 13:36:08 -0300

    Refactored fight card crawler.

--- fight_card.rb
+++ fight_card.rb
@@ -1,1 +1,25 @@
-puts "Yolo!"
+require 'mechanize'
+
+class FightCrawler
+
+  def initialize url
+    agent = Mechanize.new
+    @page = agent.get url
+  end
+
+  def fights
+    lines = fights_table.search 'tr'
+    lines.each do |line|
+      columns = line.search 'td'
+      puts "#{columns[1].text.strip} x #{columns[3].text.strip}" rescue NoMethodError 
+    end
+  end
+
+  def fights_table
+    @page.search '.toccolours'
+  end
+
+end
+
+crawler = FightCrawler.new 'https://pt.wikipedia.org/wiki/UFC_229'
+crawler.fights
